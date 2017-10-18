b1e97f031eb3b5c17f9a10c8adb040257e9197c1
FirebarSim <32907623+FirebarSim@users.noreply.github.com>
2017-10-19 00:26:24 +0100

    Added Navcom script, no content

--- Plugins/RV-6/data/modules/main.lua
+++ Plugins/RV-6/data/modules/main.lua
@@ -7,5 +7,8 @@ panelHeight3d = 1024
 components = {
 	cabin_heat {},
 	transponder {
-		position = {110,801,479,111} }
+		position = {110,801,479,111} },
+	navcom1 {
+		position = {6,916,723,100} },
+	audiopanel {}
 }
