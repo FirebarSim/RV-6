cc4c1aafa24be8d95460809f1a26f6c1018a6d19
FirebarSim <32907623+FirebarSim@users.noreply.github.com>
2017-10-19 00:11:37 +0100

    Updates to Transponder Script

--- Plugins/RV-6/data/modules/Custom Module/transponder.lua
+++ Plugins/RV-6/data/modules/Custom Module/transponder.lua
@@ -1,6 +1,7 @@
 size = {479,111}
 
-local quartz = sasl.gl.loadFont("quartz.ttf")
+local quartz = loadFont("quartz.ttf")
+local arial = loadFont("arial.ttf")
 
 createGlobalPropertyf("RV-6/data/standard_barometric_altitude",0)
 createGlobalPropertyi("RV-6/data/flight_level",0)
@@ -13,6 +14,8 @@ defineProperty("flightlevel", globalPropertyi("RV-6/data/flight_level"))
 defineProperty("vfrmode", globalPropertyi("RV-6/radio/transponder/vfr_mode"))
 defineProperty("currentcode", globalPropertyi("sim/cockpit/radios/transponder_code"))
 defineProperty("lastcode", globalPropertyi("RV-6/radio/transponder/last_code"))
+defineProperty("squwak",globalPropertyi("sim/cockpit2/radios/indicators/transponder_id"))
+defineProperty("mode",globalPropertyi("sim/cockpit2/radios/actuators/transponder_mode"))
 
 local vfrMode = sasl.createCommand("RV-6/radio/transponder/vfr_button", "Toggles VFR Mode")
 
@@ -41,7 +44,7 @@ function vfrModeHandler(phase) -- phase equals: 0 on press; 1 while holding; 2 o
 	return 0
 end
 
-sasl.registerCommandHandler(vfrMode, 0, vfrModeHandler)
+registerCommandHandler(vfrMode, 0, vfrModeHandler)
 
 function update()
 	set(altitude,145442.1609230769 * (((29.92126 / get(pressure)) ^ 0.19026643566373184768498208549422) - 1))
@@ -49,13 +52,31 @@ function update()
 	--altitude = 44330.76923076923 * (((29.92126 / pressure)
 	--top formula above includes factor to convert to feet
 	
-	set(flightlevel,round((get(altitude)/100)/5,0)*5)
+	set(flightlevel,round(get(altitude)/100,0))
 	
 	updateAll(components)
 end
 
 function draw()
 	drawAll(components)
-	drawText(quartz, 310, 36, string.format("%i",get(currentcode)), 45,false,true,TEXT_ALIGN_LEFT,quartz_colour) -- number id , number x , number y , string text , number size , boolean isBold , boolean isItalic , TextAlignment alignment , Color color
-	drawText(quartz, 80, 36, string.format("%03i",get(flightlevel)), 45,false,true,TEXT_ALIGN_LEFT,quartz_colour)
+	--drawRectangle (0, 0, 479, 111, rgbColour(0,0,0))
+	if get(mode) == 0 then
+	
+	elseif get(mode) == 4 then
+		drawText(quartz, 310, 36, "8888", 45,false,true,TEXT_ALIGN_LEFT,quartz_colour)
+		drawText(quartz, 80, 36, "888", 45,false,true,TEXT_ALIGN_LEFT,quartz_colour)
+		drawText(arial, 285, 55, "R", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
+		drawText(arial, 190, 55, "ALT", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
+		drawText(arial, 48, 30, "FL", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
+	else
+		drawText(quartz, 310, 36, string.format("%i",get(currentcode)), 45,false,true,TEXT_ALIGN_LEFT,quartz_colour) -- number id , number x , number y , string text , number size , boolean isBold , boolean isItalic , TextAlignment alignment , Color color
+		drawText(quartz, 80, 36, string.format("%03i",get(flightlevel)), 45,false,true,TEXT_ALIGN_LEFT,quartz_colour)
+		if get(squwak) == 1 then
+			drawText(arial, 285, 55, "R", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
+		end
+		if get(mode) == 3 then
+			drawText(arial, 190, 55, "ALT", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
+		end
+		drawText(arial, 48, 30, "FL", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
+	end
 end
