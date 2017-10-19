b695527bd3af517cc0c1c395fb4db0c8f398133d
FirebarSim <32907623+FirebarSim@users.noreply.github.com>
2017-10-20 00:55:55 +0100

    3D Attitude indicator, some electrical wizardry

--- Plugins/RV-6/data/modules/Custom Module/transponder.lua
+++ Plugins/RV-6/data/modules/Custom Module/transponder.lua
@@ -16,6 +16,9 @@ defineProperty("currentcode", globalPropertyi("sim/cockpit/radios/transponder_co
 defineProperty("lastcode", globalPropertyi("RV-6/radio/transponder/last_code"))
 defineProperty("squwak",globalPropertyi("sim/cockpit2/radios/indicators/transponder_id"))
 defineProperty("mode",globalPropertyi("sim/cockpit2/radios/actuators/transponder_mode"))
+defineProperty("avionicsswitch",globalPropertyi("sim/cockpit2/switches/avionics_power_on"))
+defineProperty("bus2power",globalPropertyfae("sim/cockpit2/electrical/bus_volts",2))
+defineProperty("bus2extra",globalPropertyfae("sim/cockpit2/electrical/plugin_bus_load_amps",2))
 
 local vfrMode = sasl.createCommand("RV-6/radio/transponder/vfr_button", "Toggles VFR Mode")
 
@@ -54,29 +57,35 @@ function update()
 	
 	set(flightlevel,round(get(altitude)/100,0))
 	
+	if get(mode) == 0 then
+		set(bus2extra,-0.5)
+	else
+		set(bus2extra,0)
+	end
+	
 	updateAll(components)
 end
 
 function draw()
 	drawAll(components)
 	drawRectangle (0, 0, 479, 111, rgbColour(0,0,0))
-	if get(mode) == 0 then
-	
-	elseif get(mode) == 4 then
-		drawText(quartz, 310, 36, "8888", 45,false,true,TEXT_ALIGN_LEFT,quartz_colour)
-		drawText(quartz, 80, 36, "888", 45,false,true,TEXT_ALIGN_LEFT,quartz_colour)
-		drawText(arial, 285, 55, "R", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
-		drawText(arial, 190, 55, "ALT", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
-		drawText(arial, 48, 30, "FL", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
-	else
-		drawText(quartz, 310, 36, string.format("%i",get(currentcode)), 45,false,true,TEXT_ALIGN_LEFT,quartz_colour) -- number id , number x , number y , string text , number size , boolean isBold , boolean isItalic , TextAlignment alignment , Color color
-		drawText(quartz, 80, 36, string.format("%03i",get(flightlevel)), 45,false,true,TEXT_ALIGN_LEFT,quartz_colour)
-		if get(squwak) == 1 then
+	if get(avionicsswitch) == 1 and get(bus2power) > 11 then
+		if get(mode) == 4 then
+			drawText(quartz, 310, 36, "8888", 45,false,true,TEXT_ALIGN_LEFT,quartz_colour)
+			drawText(quartz, 80, 36, "888", 45,false,true,TEXT_ALIGN_LEFT,quartz_colour)
 			drawText(arial, 285, 55, "R", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
-		end
-		if get(mode) == 3 then
 			drawText(arial, 190, 55, "ALT", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
+			drawText(arial, 48, 30, "FL", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
+		elseif get(mode) == 1 or get(mode) == 2 or get(mode) == 3 or get(mode) == 5 then
+			drawText(quartz, 310, 36, string.format("%i",get(currentcode)), 45,false,true,TEXT_ALIGN_LEFT,quartz_colour) -- number id , number x , number y , string text , number size , boolean isBold , boolean isItalic , TextAlignment alignment , Color color
+			drawText(quartz, 80, 36, string.format("%03i",get(flightlevel)), 45,false,true,TEXT_ALIGN_LEFT,quartz_colour)
+			if get(squwak) == 1 then
+				drawText(arial, 285, 55, "R", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
+			end
+			if get(mode) == 3 then
+				drawText(arial, 190, 55, "ALT", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
+			end
+			drawText(arial, 48, 30, "FL", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
 		end
-		drawText(arial, 48, 30, "FL", 14,true,false,TEXT_ALIGN_LEFT,quartz_colour)
 	end
 end
