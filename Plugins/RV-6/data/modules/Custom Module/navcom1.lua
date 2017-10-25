b4c2c9527d2cb0d37060e1fb6807a507566d9642
FirebarSim <32907623+FirebarSim@users.noreply.github.com>
2017-10-25 22:45:07 +0100

    NavCom

--- Plugins/RV-6/data/modules/Custom Module/navcom1.lua
+++ Plugins/RV-6/data/modules/Custom Module/navcom1.lua
@@ -8,8 +8,10 @@ local sevenSegment = loadFont("7Segment.otf")
 
 createGlobalPropertyi("RV-6/radio/navcom/com1_mode",1) -- 1 = normal, 2 = active entry, 3 = channel select, 4 = channel program, 
 createGlobalPropertyi("RV-6/radio/navcom/com1_last_mode",0)
+createGlobalPropertyi("RV-6/radio/navcom/com1_fineout",0)
 createGlobalPropertyfa("RV-6/radio/navcom/com1_ch",32) -- saved com channels
-createGlobalPropertyi("RV-6/radio/navcom/nav1_mode",1) -- 1 = normal, 2 = active entry
+createGlobalPropertyi("RV-6/radio/navcom/nav1_mode",1)
+createGlobalPropertyi("RV-6/radio/navcom/nav1_fineout",0) -- 1 = normal, 2 = active entry
 createGlobalPropertyf("RV-6/radio/navcom/total_navcom_time_sec",0)
 
 defineProperty("com1freq", globalPropertyf("sim/cockpit2/radios/actuators/com1_frequency_hz"))
@@ -17,6 +19,7 @@ defineProperty("com1stby", globalPropertyf("sim/cockpit2/radios/actuators/com1_s
 defineProperty("com1mode", globalPropertyi("RV-6/radio/navcom/com1_mode"))
 defineProperty("com1power",globalPropertyi("sim/cockpit2/radios/actuators/com1_power"))
 defineProperty("com1last", globalPropertyi("RV-6/radio/navcom/com1_last_mode"))
+com1ch = globalPropertyfa("RV-6/radio/navcom/com1_ch")
 defineProperty("nav1freq", globalPropertyf("sim/cockpit2/radios/actuators/nav1_frequency_hz"))
 defineProperty("nav1stby", globalPropertyf("sim/cockpit2/radios/actuators/nav1_standby_frequency_hz"))
 defineProperty("nav1mode", globalPropertyi("RV-6/radio/navcom/nav1_mode"))
@@ -29,11 +32,16 @@ defineProperty("globaltime",globalPropertyf("sim/time/total_flight_time_sec"))
 defineProperty("timeperiod", globalPropertyf("sim/operation/misc/frame_rate_period"))
 defineProperty("elapsedtime",globalPropertyf("RV-6/radio/navcom/total_navcom_time_sec"))
 
+local channel_number = 1
+
 set(nav1power,get(com1power)) -- Initialise Power Values
 
 function rgbColour(r,g,b)
 	return {r/255,g/255,b/255}
 end
+
+local quartz_colour = rgbColour(255, 153, 0)
+local shadow_colour = rgbColour(32,19,0)
 
 function navcomPowerHandler(phase)
 	if phase == 0 then
@@ -59,17 +67,29 @@ function com1TransferHandler(phase)
 		if get(globaltime) - temp > 2 and flips < 1 then
 			if get(com1mode) == 1 then
 				set(com1mode,2)
-			else
+			elseif get(com1mode) == 2 then
 				set(com1mode,1)
+			elseif get(com1mode) == 3 then
+				set(com1freq,get(com1ch,channel_number))
+			elseif get(com1mode) == 4 then
+				set(com1mode,5)
+			elseif get(com1mode) == 5 then
+				set(com1mode,4)
 			end
 			flips = 1
 		end
 	elseif phase == 2 then
 		if get(globaltime) - temp < 2 then
-			if get(com1mode) == 2 then
-				set(com1mode,1)
-			else
+			if get(com1mode) == 1 then
 				commandOnce(com1Flip)
+			elseif get(com1mode) == 2 then
+				set(com1mode,1)
+			elseif get(com1mode) == 3 then
+				set(com1freq,get(com1ch,channel_number))
+			elseif get(com1mode) == 4 then
+				set(com1mode,5)
+			elseif get(com1mode) == 5 then
+				set(com1mode,4)
 			end
 		end
 	end
@@ -80,12 +100,19 @@ registerCommandHandler(com1Transfer,0, com1TransferHandler)
 
 function com1CoarseUpHandler(phase)
 	if phase == 0 then
-		if get(com1mode) == 0 then
-		
-		elseif get(com1mode) == 1 then
+		if get(com1mode) == 1 then
 			commandOnce(findCommand("sim/radios/stby_com1_coarse_up"))
 		elseif get(com1mode) == 2 then
 			commandOnce(findCommand("sim/radios/actv_com1_coarse_up"))
+		elseif get(com1mode) == 5 then
+			--Program Freq
+			if get(com1ch,channel_number - 1) == 0 then
+				set(com1ch,11800,channel_number - 1)
+			elseif get(com1ch,channel_number - 1) >= 13600 then
+				set(com1ch,0,channel_number - 1)
+			else
+				set(com1ch,get(com1ch,channel_number) + 100,channel_number - 1)
+			end
 		end
 	end
 end
@@ -95,12 +122,19 @@ registerCommandHandler(com1CoarseUp,0, com1CoarseUpHandler)
 
 function com1CoarseDnHandler(phase)
 	if phase == 0 then
-		if get(com1mode) == 0 then
-		
-		elseif get(com1mode) == 1 then
+		if get(com1mode) == 1 then
 			commandOnce(findCommand("sim/radios/stby_com1_coarse_down"))
 		elseif get(com1mode) == 2 then
 			commandOnce(findCommand("sim/radios/actv_com1_coarse_down"))
+		elseif get(com1mode) == 5 then
+			--Program Freq
+			if get(com1ch,channel_number - 1) == 0 then
+				set(com1ch,13600,channel_number - 1)
+			elseif get(com1ch,channel_number - 1) <= 11800 then
+				set(com1ch,0,channel_number - 1)
+			else
+				set(com1ch,get(com1ch,channel_number) - 100,channel_number - 1)
+			end
 		end
 	end
 end
@@ -110,12 +144,27 @@ registerCommandHandler(com1CoarseDn,0, com1CoarseDnHandler)
 
 function com1FineUpHandler(phase)
 	if phase == 0 then
-		if get(com1mode) == 0 then
-		
-		elseif get(com1mode) == 1 then
+		if get(com1mode) == 1 then
 			commandOnce(findCommand("sim/radios/stby_com1_fine_up"))
 		elseif get(com1mode) == 2 then
 			commandOnce(findCommand("sim/radios/actv_com1_fine_up"))
+		elseif get(com1mode) == 3 and channel_number < 32 then
+			channel_number = channel_number + 1
+		elseif get(com1mode) == 4 and channel_number < 32 then
+			channel_number = channel_number + 1
+		elseif get(com1mode) == 5 then
+			--Program Freq
+			if get(com1ch,channel_number - 1) == 0 then
+				set(com1ch,11800,channel_number - 1)
+			elseif get(com1ch,channel_number - 1) >= 13697 then
+				set(com1ch,0,channel_number - 1)
+			else
+				if get(com1ch,channel_number - 1) % 5 == 0 then
+					set(com1ch,get(com1ch,channel_number) + 2,channel_number - 1)
+				else
+					set(com1ch,get(com1ch,channel_number) + 3,channel_number - 1)
+				end
+			end
 		end
 	end
 end
@@ -125,12 +174,23 @@ registerCommandHandler(com1FineUp,0, com1FineUpHandler)
 
 function com1FineDnHandler(phase)
 	if phase == 0 then
-		if get(com1mode) == 0 then
-		
-		elseif get(com1mode) == 1 then
+		if get(com1mode) == 1 then
 			commandOnce(findCommand("sim/radios/stby_com1_fine_down"))
 		elseif get(com1mode) == 2 then
 			commandOnce(findCommand("sim/radios/actv_com1_fine_down"))
+		elseif get(com1mode) == 3 and channel_number > 1 then
+			channel_number = channel_number - 1
+		elseif get(com1mode) == 4 and channel_number < 32 then
+			channel_number = channel_number - 1
+		elseif get(com1mode) == 5 then
+			--Program Freq
+			if get(com1ch,channel_number - 1) == 0 then
+				set(com1ch,13697,channel_number - 1)
+			elseif get(com1ch,channel_number) <= 11800 then
+				set(com1ch,0,channel_number)
+			else
+				set(com1ch,get(com1ch,channel_number - 1) - 2.5,channel_number - 1)
+			end
 		end
 	end
 end
@@ -139,24 +199,44 @@ local com1FineDn = createCommand("RV-6/radio/navcom/com1_fine_dn", "Small Knob D
 registerCommandHandler(com1FineDn,0, com1FineDnHandler)
 
 function com1ChannelHandler(phase)
---	if phase == 0 then
---		temp = get(globaltime)
---		flips = 0
---	elseif phase == 1 then
---		if get(globaltime) - temp > 2 and flips < 1 then
---			if get(com1mode) == 1 or get(com1mode) == 2 then
---				set(com1last,get(com1mode))
---				set(com1mode,3)
---			else
---				set(com1mode,get(com1last))
---			end
---			flips = 1
---		end
---	elseif phase == 2 then
---		if get(com1mode) == 3 then
---			set(com1mode,get(com1last))
---		end
---	end
+	if phase == 0 then
+		temp = get(globaltime)
+		flips = 0
+	elseif phase == 1 then
+		if get(globaltime) - temp > 2 and flips < 1 then
+			if get(com1mode) == 1 then
+				set(com1last,get(com1mode))
+				set(com1mode,4)
+			elseif get(com1mode) == 2 then
+				set(com1last,get(com1mode))
+				set(com1mode,4)
+			elseif get(com1mode) == 3 then
+				set(com1last,get(com1mode))
+				set(com1mode,4)
+			elseif get(com1mode) == 4 then
+				set(com1mode,get(com1last))
+			elseif get(com1mode) == 5 then
+				--Nothing
+			end
+			flips = 1
+		end
+	elseif phase == 2 then
+		if get(globaltime) - temp < 2 then
+			if get(com1mode) == 1 then
+				set(com1last,get(com1mode))
+				set(com1mode,3)
+			elseif get(com1mode) == 2 then
+				set(com1last,get(com1mode))
+				set(com1mode,3)
+			elseif get(com1mode) == 3 then
+				set(com1mode,1)
+			elseif get(com1mode) == 4 then
+				set(com1mode,get(com1last))
+			elseif get(com1mode) == 5 then
+				--Nothing
+			end
+		end
+	end
 end
 
 local com1Channel = createCommand("RV-6/radio/navcom/com1_channel", "Small Knob Down")
@@ -254,15 +334,13 @@ end
 local nav1FineDn = createCommand("RV-6/radio/navcom/nav1_fine_dn", "Small Knob Down")
 registerCommandHandler(nav1FineDn,0, nav1FineDnHandler)
 
-local quartz_colour = rgbColour(255, 153, 0)
-local shadow_colour = rgbColour(32,19,0)
-
 function update()
 	if get(com1power) == 1 then
 		set(elapsedtime,get(elapsedtime)+get(timeperiod))
 	else
 		set(elapsedtime,0)
 	end
+	
 	updateAll(components)
 end
 
@@ -283,7 +361,24 @@ function draw()
 		--Mode Displays
 		if get(com1mode) == 1 then
 			drawText(sevenSegment,215,55,string.format("%6.2f",get(com1stby)/100),30,false,true,TEXT_ALIGN_LEFT,quartz_colour)
+		elseif get(com1mode) == 1 then
+			--Nothing
+		elseif get(com1mode) == 3 then
+			drawText(sevenSegment,215,55,string.format("%06.2f",get(com1ch,channel_number - 1)/100),30,false,true,TEXT_ALIGN_LEFT,quartz_colour)
+			drawText(sixteenSegment,215,15," CH:" .. channel_number .. "",30,false,false,TEXT_ALIGN_LEFT,quartz_colour)
+		elseif get(com1mode) == 4 then
+			drawText(sevenSegment,215,55,string.format("%06.2f",get(com1ch,channel_number - 1)/100),30,false,true,TEXT_ALIGN_LEFT,quartz_colour)
+			drawText(sixteenSegment,215,15," PG:",30,false,false,TEXT_ALIGN_LEFT,quartz_colour)
+			if get(globaltime) % 1 > 0.5 then
+				drawText(sixteenSegment,215,15,"   :" .. channel_number .. "",30,false,false,TEXT_ALIGN_LEFT,quartz_colour)
+			end
+		elseif get(com1mode) == 5 then
+			if get(globaltime) % 1 > 0.5 then
+				drawText(sevenSegment,215,55,string.format("%06.2f",get(com1ch,channel_number - 1)/100),30,false,true,TEXT_ALIGN_LEFT,quartz_colour)
+			end
+			drawText(sixteenSegment,215,15," PG:" .. channel_number .. "",30,false,false,TEXT_ALIGN_LEFT,quartz_colour)
 		end
+		
 		if get(nav1mode) == 1 then
 			--Normal Mode
 			--drawText(sevenSegment,575,55,string.format("%6.2f",get(nav1stby)/100),30,false,true,TEXT_ALIGN_LEFT,quartz_colour)
